b9bb5a3b8096fb717b61fb74aa94423a77444fe0
hossseinmh <akhoundi22@gmail.com>
2017-03-21 15:30:32 +0330

    add a choices attribute to a one colume in database

--- djmod/modlearn/web/models.py
+++ djmod/modlearn/web/models.py
@@ -2,13 +2,17 @@ from django.db import models
 from django.db.models import Model
 # Create your models here.
 
-
+PUBLISH_CHOICES = (
+    ('draft','Draft'),
+    ('send','Send'),
+)
 class Post(Model):
     id =models.BigAutoField(primary_key=True)
     active = models.BooleanField(default= True) # can empty in the database
     title = models.CharField(max_length=240 , default='new post' ,verbose_name='post title')
     # add a verbose name to show a name on view but didnt change a name in database
-    content = models.TextField(null=True , blank=True)
+    content = models.TextField(null=True, blank=True)
+    publish = models.CharField(max_length=120,choices=PUBLISH_CHOICES , default='draft')
     class Meta:
         verbose_name = 'postHa'
         verbose_name_plural = 'addPost'
