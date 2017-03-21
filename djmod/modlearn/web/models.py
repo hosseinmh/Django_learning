ae455ee8762235fc90c26388b552993805865cc6
hossseinmh <akhoundi22@gmail.com>
2017-03-21 15:01:33 +0330

    add a verbose name and use a blank attribute

--- djmod/modlearn/web/models.py
+++ djmod/modlearn/web/models.py
@@ -6,5 +6,8 @@ from django.db.models import Model
 class Post(Model):
     id =models.BigAutoField(primary_key=True)
     active = models.BooleanField(default= True) # can empty in the database
-    title = models.CharField(max_length=240 , default='new post')
+    title = models.CharField(max_length=240 , default='new post' ,verbose_name='post title')
+    # add a verbose name to show a name on view but didnt change a name in database
     content = models.TextField(null=True , blank=True)
+    pass
+
